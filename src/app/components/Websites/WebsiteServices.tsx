69157d9fb4581a679df151088a5ffaf69e107b17
samanhoseinpour <samangithoseinpour@gmail.com>
2025-09-06 19:42:57 +0330

    feat: website services + testimonial component, fix: build error

---
+++ src/app/components/Websites/WebsiteServices.tsx
@@ -0,0 +1,11 @@
+import { Container } from "../";
+
+const WebsiteServices = () => {
+  return (
+    <section>
+      <Container>WebsiteServices</Container>
+    </section>
+  );
+};
+
+export default WebsiteServices;
---
+++ src/app/components/Websites/WebsiteTestimonials.tsx
@@ -0,0 +1,11 @@
+import { Container } from "../";
+
+const WebsiteTestimonials = () => {
+  return (
+    <section>
+      <Container>WebsiteTestimonials</Container>
+    </section>
+  );
+};  
+
+export default WebsiteTestimonials;
