946d30ac6771899adfaec963c1a6ef700a364504
samanhoseinpour <samangithoseinpour@gmail.com>
2025-09-14 11:37:01 +0330

    feat: implement blogs grid

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,5 +1,11 @@
 import { BlogsParallaxHero, BlogGrid } from "../components";
 
+export const metadata = {
+  title: "Blogs | Perseus Creative Studio",
+  description:
+    "Explore the latest news, insights, and updates from Perseus Creative Studio. about Marketing, Visual Production, and Website Development & Design",
+};
+
 const BlogsPage = () => {
   return (
     <main>
