7bc4b5a8bc405357acc995d0a9b718639d5f2018
samanhoseinpour <samangithoseinpour@gmail.com>
2025-12-13 23:43:40 +0330

    generate sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,49 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.perseustudio.com/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+      images: ["https://ik.imagekit.io/perseus/logo-black.png"],
+    },
+    {
+      url: "https://www.perseustudio.com/services",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.9,
+    },
+    {
+      url: "https://www.perseustudio.com/projects",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.perseustudio.com/about",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.7,
+    },
+    {
+      url: "https://www.perseustudio.com/services/websites",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.6,
+    },
+    {
+      url: "https://www.perseustudio.com/contact",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.perseustudio.com/blogs",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.4,
+    },
+  ];
+}
