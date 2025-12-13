5021994e058ea69aab0d466451811fcbecde88b9
samanhoseinpour <samangithoseinpour@gmail.com>
2025-12-13 23:47:32 +0330

    fix: ui iss

--- src/app/components/Home/Hero.tsx
+++ src/app/components/Home/Hero.tsx
@@ -29,37 +29,35 @@ const Hero = () => {
           className="sticky top-0 h-screen object-cover w-full"
         />
       </motion.div>
-      <div className="px-6">
-        <Container className="relative z-10 h-svh pb-7">
-          <motion.div
-            className="flex h-full flex-col items-start justify-end text-white"
-            variants={{
-              hidden: { opacity: 0 },
-              visible: { opacity: 1 },
-            }}
-            whileInView="visible"
-            exit="hidden"
-            animate="hidden"
-            viewport={{ amount: 0.98 }}
-          >
-            <h1 className="text-4xl leading-4xl sm:text-5xl sm:leading-5xl font-bold">
-              Perseus Creative Studio.
-            </h1>
+      <Container className="relative z-10 h-svh pb-7">
+        <motion.div
+          className="flex h-full flex-col items-start justify-end text-white"
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1 },
+          }}
+          whileInView="visible"
+          exit="hidden"
+          animate="hidden"
+          viewport={{ amount: 0.98 }}
+        >
+          <h1 className="text-4xl leading-4xl sm:text-5xl sm:leading-5xl font-bold">
+            Perseus Creative Studio.
+          </h1>
 
-            <h2 className="text-3xl leading-3xl sm:text-4xl sm:leading-4xl font-semibold mb-10">
-              We help you build a brand people love.
-            </h2>
+          <h2 className="text-3xl leading-3xl sm:text-4xl sm:leading-4xl font-semibold mb-10">
+            We help you build a brand people love.
+          </h2>
 
-            <Link href="/contact" className="mb-16">
-              <Button size="large">Get Started</Button>
-            </Link>
-            <h2 className="font-semibold text-sm leading-sm">
-              We’re a creative marketing studio focused on helping businesses
-              grow with purpose.
-            </h2>
-          </motion.div>
-        </Container>
-      </div>
+          <Link href="/contact" className="mb-16">
+            <Button size="large">Get Started</Button>
+          </Link>
+          <h2 className="font-semibold text-sm leading-sm">
+            We’re a creative marketing studio focused on helping businesses grow
+            with purpose.
+          </h2>
+        </motion.div>
+      </Container>
     </section>
   );
 };
