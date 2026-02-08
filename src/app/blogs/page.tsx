5fae90e9ec8213d645b43657e507aebbf44ba202
samanhoseinpour <samangithoseinpour@gmail.com>
2026-02-08 17:21:13 +0330

    fix: missed metadata

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,4 +1,34 @@
+import { Metadata } from 'next';
 import { BlogGrid } from '../components';
+
+export const metadata: Metadata = {
+  title: 'Blogs & Digital Marketing Insights - Perseus Creative Studio',
+  description:
+    'In Perseus Creative Studio blog we share our digital marketing insights, fresh case studies for you to stay one step ahead in your business growth.',
+  keywords: ['Vancouver Digital Marketing Agency'],
+
+  alternates: {
+    canonical: 'https://www.perseustudio.com/blogs',
+  },
+
+  openGraph: {
+    title: 'Blogs & Digital Marketing Insights - Perseus Creative Studio',
+    description:
+      'In Perseus Creative Studio blog we share our digital marketing insights, fresh case studies for you to stay one step ahead in your business growth.',
+    url: 'https://www.perseustudio.com/blogs',
+    siteName: 'Perseus Creative Studio',
+    locale: 'en_US',
+    type: 'website',
+    images: [
+      {
+        url: 'https://ik.imagekit.io/perseus/logo-black.png',
+        width: 1200,
+        height: 630,
+        alt: 'Perseus Creative Studio About Page Preview',
+      },
+    ],
+  },
+};
 
 const BlogsPage = () => {
   return (
