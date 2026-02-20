fce89a7f02542a0036f29f4025a49a465d757025
samanhoseinpour <samangithoseinpour@gmail.com>
2026-02-20 22:54:34 +0330

    enable keep alive

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,35 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: "/web-development",
-        destination: "/services/websites",
+        source: '/web-development',
+        destination: '/services/websites',
         permanent: true,
       },
       {
-        source: "/visual-production",
-        destination: "/projects",
+        source: '/visual-production',
+        destination: '/projects',
         permanent: true,
       },
       {
         source: '/blogs/web-development/strong-website-vancouver-business',
         destination: '/blogs/strong-website-vancouver-business',
         permanent: true,
-      }
+      },
     ];
   },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "ik.imagekit.io",
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
       },
     ],
+  },
+  httpAgentOptions: {
+    keepAlive: true,
   },
 };
 
